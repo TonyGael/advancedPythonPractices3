2e0ac3de5af11997bd5580de508787b99433a419
TonyGael <anthonygaelmayol@gmail.com>
2024-03-23 01:43:40 -0300

    adding some little changes to main script

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 # secuencias
 
-# def mygenerator(n):
+# def my_generator(n): #  lazy executions
 #     for x in range(n):
 #         yield x**3
-
-
-# values = mygenerator(9000000)
 #
+#
+# values = my_generator(20)
+
 # print(next(values))
 # print(next(values))
 # print(next(values))
@@ -14,14 +14,26 @@
 # print(next(values))
 # print(next(values))
 
-# import sys
-
-# values = mygenerator(20)
-# print(sys.getsizeof(values))
 # for x in values:
 #     print(x)
 
-def infinite_sequence(): # controled by yield
+############################################################3
+
+# import sys
+#
+# def my_generator_1(n):
+#     for x in range(n):
+#         yield x**3
+#
+#
+# # values = my_generator_1(100)  # prints ou 104 bits
+# values = my_generator_1(500000)  # prints ou 104 bits
+#
+# print(sys.getsizeof(values))
+
+################################################################
+
+def infinite_sequence():  # controled by yield
     result = 1
     while True:
         yield result
@@ -36,5 +48,8 @@ values = infinite_sequence()
 # print(next(values))
 # print(next(values))
 
-for x in values:
-    print(x)
+# for x in values:
+#     print(x)  # houston there is a problem here!
+
+for x in range(20):
+    print(next(values))
